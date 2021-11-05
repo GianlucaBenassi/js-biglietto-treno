f53f0a934ad416529f2bd79f6ca0cc137798ef14
Gianluca Benassi <benox1199@gmail.com>
2021-11-05 14:28:38 +0100

    number validation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
-const km = prompt('Inserisci i km che percorrerai:');
-const age = prompt('Inserisci la tua età:')
-
+let km;
+let age;
 let price;
 
+// ask km until correct number
+do {
+
+    km = prompt('Inserisci i km che percorrerai:');
+    if (isNaN(km) || km <= 0) {
+        alert('Inserisci un valore per i km valido!');
+    }
+
+} while (isNaN(km) || km <= 0);
+
+// ask age until valid
+do {
+
+    age = prompt('Inserisci la tua età:')
+    if (isNaN(age) || age < 0) {
+        alert("Inserisci un valore per l'età valido!");
+    }
+    
+} while (isNaN(age) || age < 0);
+
+// price calculation
 if (age < 18) {
     price = km * 0.21 * 80 / 100
 } else if (age > 65) {
