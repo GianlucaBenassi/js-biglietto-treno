e66f2856f17ec1577f8615b5dea5275e2e1c9139
Gianluca Benassi <benox1199@gmail.com>
2021-11-05 14:04:08 +0100

    price calculation

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const km = prompt('Inserisci i km che percorrerai:');
+const age = prompt('Inserisci la tua età:')
+
+let price;
+
+if (age < 18) {
+    price = km * 0.21 * 80 / 100
+} else if (age > 65) {
+    price = km * 0.21 * 60 / 100
+} else {
+    price = km * 0.21
+}
+
+console.log(price);
